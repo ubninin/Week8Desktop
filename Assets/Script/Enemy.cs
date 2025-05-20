0fd391681823f5061473aad82a083c02f0e4d591
ubninin <144878603+ubninin@users.noreply.github.com>
2025-05-21 05:05:16 +0900

    monster

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+public enum EnemyDestroyType { Kill = 0, Arrive }
+public class Enemy : MonoBehaviour
+{
+    private Transform[] wayPoints;
+    private int wayPointCount;
+    private int currentIndex = 0;
+    private Movement2D movement2D;
+    //private EnemySpawner enemySpawner;
+    //[SerializeField] private int gold = 10;
+    public void Setup(Transform[] wayPoints)
+    {
+        movement2D = GetComponent<Movement2D>();
+        //this.enemySpawner = enemySpawner;
+        wayPointCount = wayPoints.Length;
+        this.wayPoints = new Transform[wayPointCount];
+        this.wayPoints = wayPoints;
+        transform.position = wayPoints[currentIndex].position;
+        StartCoroutine("OnMove");
+    }
+
+    private IEnumerator OnMove()
+    {
+        while (true)
+        {
+            if (Vector3.Distance(transform.position, wayPoints[currentIndex].position) < 0.02f * movement2D.MoveSpeed)
+            {
+                NextMoveTo();
+            }
+
+            yield return null;
+        }
+    }
+
+    private void NextMoveTo()
+    {
+        if (currentIndex < wayPointCount - 1)
+        {
+            transform.position = wayPoints[currentIndex].position;
+            currentIndex++;
+            Vector3 direction = (wayPoints[currentIndex].position - transform.position).normalized;
+            movement2D.MoveTo(direction);
+        }
+        else
+        {
+            Destroy(gameObject);
+            //gold = 0;
+            //OnDie(EnemyDestroyType.Arrive);
+        }
+    }
+    //public void OnDie(EnemyDestroyType type)
+    //{
+    //    enemySpawner.DestroyEnemy(type, this,gold);
+    //}
+}
